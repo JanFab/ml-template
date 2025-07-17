9195a42dc7471dcc207e0426135d009fdb2ace5e
JanFabBlue <jan.fabian@bluelemons.sk>
2025-07-17 13:01:13 +0200

    refactor

--- scripts/run_training_podman.ps1
+++
@@ -1,9 +0,0 @@
-# Build the container
-podman build -t ml-training -f containers/ml-service/Dockerfile .
-
-# Run the container with volume mounts for data and MLflow
-podman run -it --rm `
-    -v ${PWD}/data:/app/data `
-    -v ${PWD}/mlruns:/app/mlruns `
-    -v ${PWD}/configs:/app/configs `
-    ml-training 
